dee75db962ca0231a3cade834dfe75227ea59d4d
Rowa <jrowah@gmail.com>
2022-07-22 10:01:21 +0300

    sets up ejs

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.send(`Hello from YellCamp!`);
+  res.render(`home`);
 });
 
 app.listen(PORT, () => {
