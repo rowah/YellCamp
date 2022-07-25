f3fd29a361553cd869cfd46587ea048dec33bf21
Rowa <jrowah@gmail.com>
2022-07-25 22:55:24 +0300

    sets up mongoose model

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
 const path = require("path");
+const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+
+// //connects mongodb and passes in options to prevent yelling
+// mongoose.connect("mongodb://localhost:27017/yell-camp", {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// });
+
+// //checks errors in db connections and alerts error or successful opening
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error!"));
+// db.once("open", () => {
+//   console.log(`Database Connected!!!`);
+// });
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
