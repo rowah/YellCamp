64d146b3f57dc1277d9113d028bdc2faedf5b369
Rowa <jrowah@gmail.com>
2022-07-26 10:13:34 +0300

    hard codes new campground

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
+const Campground = require("./models/campground");
 
 //connects mongodb and passes in options to prevent yelling
 mongoose.connect("mongodb://localhost:27017/yell-camp", {
@@ -23,6 +24,18 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render(`home`);
+});
+
+//makes new campround through hardcoding
+app.get("/makenewcampground", async (req, res) => {
+  const camp = new Campground({
+    title: "Matangi",
+    price: 200,
+    description: "The Cheapest ever!",
+    location: "Ruiru, Nairobi",
+  });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(PORT, () => {
